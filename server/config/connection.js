32b90f987854e6791e327dddaa1c927b36619e36
Ridercogswell <ridercogswell@gmail.com>
2022-06-08 22:45:12 -0700

    typedefs

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,8 +4,7 @@ mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+
 });
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
-// const routes = require('./routes');
-
+const { typeDefs, resolvers } = require('./schemas')
 const { authMiddleware } = require('./utils/auth')
 
 const app = express();
